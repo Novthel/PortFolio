d7d0cb8c5b7c6c6fb3c929ebb7d27dcdb67f38ea
oscar novoa <novthel@live.com>
2022-05-05 14:16:13 -0500

    agrega enlaces de proyecto

--- src/components/grid/ProyectGrid.jsx
+++ src/components/grid/ProyectGrid.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ExternalLink } from 'react-external-link';
 import styles from './ProyectGrid.module.css';
 import cine from '../../media/cine.jpg';
 import veterinaria from '../../media/veterinaria.jpg';
@@ -10,20 +11,29 @@ export default function ProyectGrid() {
         <div className={styles.subTitle} >
           <h3>Mis Proyectos</h3>
         </div>
+
         <div className={styles.card}>
-          <img src={cine} width={300} height={200} alt='cine'/>
+          <ExternalLink href="https://novthel-search-movies.netlify.app/">
+            <img src={cine} width={300} height={200} alt='cine'/>
+          </ExternalLink>
           <div className={styles.title}>
             <p>Buscador de Peliculas</p>
           </div>
         </div>
+
         <div className={styles.card}>
-          <img src={veterinaria} width={300} height={200} alt='veterinaria'/>
+          <ExternalLink href="https://novthel-administrador-citas-veterinaria.netlify.app/">
+            <img src={veterinaria} width={300} height={200} alt='veterinaria'/>
+          </ExternalLink>
           <div className={styles.title}>
             <p>Citas medicas</p>
           </div>
         </div>
+
         <div className={styles.card}>
-          <img src={criptomoneda} width={300} height={200} alt='criptomoneda'/>
+          <ExternalLink href="https://novthel-cotizador-criptomonedas.netlify.app/">
+            <img src={criptomoneda} width={300} height={200} alt='criptomoneda'/>
+          </ExternalLink>
           <div className={styles.title}>
             <p>Cotizador de Criptomoneda</p>
           </div>
