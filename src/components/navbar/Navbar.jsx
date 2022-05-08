5fd31064ec6c8075ac5b98639fcb7e57f03660c0
oscar novoa <novthel@live.com>
2022-05-08 18:34:22 -0500

    mejora navbar y responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,6 @@ export default function Navbar() {
 
     const item = document.querySelector('ul')
     item.classList.toggle(`${ styles.show }`)
-    console.log(item)
   }
 
 
@@ -18,10 +17,10 @@ export default function Navbar() {
           Novthel    
         </div>
         <ul className={ styles.itemsMenu }>
-          <li> <a href='#inicio'>Home</a></li>
-          <li> <a href='#about'>Acerca</a></li>
-          <li> <a href='#proyects'>Proyectos</a></li>
-          <li> <a href='#footer'>Contacto</a> </li>
+          <li> <a href='#inicio' onClick={show}>Home</a></li>
+          <li> <a href='#about' onClick={show}>Acerca</a></li>
+          <li> <a href='#proyects' onClick={show}>Proyectos</a></li>
+          <li> <a href='#footer' onClick={show}>Contacto</a> </li>
         </ul>
         <span className={styles.btnMenu} onClick={show}>
           <FcMenu/>
